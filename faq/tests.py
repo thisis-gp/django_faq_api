59fe900e67372ef58521159ac2cf17b4ccb3ef05
Gurupriyan D A <am400718@gmail.com>
2025-02-02 16:29:39 +0530

    test: add more unit test cases

--- faq/tests.py
+++ faq/tests.py
@@ -1,9 +1,25 @@
 from django.test import TestCase
 from .models import FAQ
 
-# Create your tests here.
 class FAQModelTest(TestCase):
-    def test_translation(self):
-        faq = FAQ(question="What is Django?", answer="Django is a web framework.")
-        faq.save()
-        self.assertEqual(faq.get_translated_question('ml'), faq.question_ml)
+    def setUp(self):
+        self.faq = FAQ.objects.create(
+            question="What is Django?",
+            answer="Django is a web framework."
+        )
+
+    def test_english_translation(self):
+        self.assertEqual(self.faq.get_translated_question('en'), "What is Django?")
+
+    def test_malayalam_translation(self):
+        self.faq.question_ml = "ഡjango എന്താണ്?"
+        self.faq.save()
+        self.assertEqual(self.faq.get_translated_question('ml'), "ഡjango എന്താണ്?")
+
+    def test_tamil_translation(self):
+        self.faq.question_ta = "டjango என்றால் என்ன?"
+        self.faq.save()
+        self.assertEqual(self.faq.get_translated_question('ta'), "டjango என்றால் என்ன?")
+
+    def test_fallback_to_english(self):
+        self.assertEqual(self.faq.get_translated_question('invalid_lang'), "What is Django?")
