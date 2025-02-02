7ee560a6151aeb5c2ee01331f11c25d298bd4f52
Gurupriyan D A <am400718@gmail.com>
2025-02-02 16:22:14 +0530

    test: add initial unit test

--- faq/tests.py
+++ faq/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import FAQ
 
 # Create your tests here.
+class FAQModelTest(TestCase):
+    def test_translation(self):
+        faq = FAQ(question="What is Django?", answer="Django is a web framework.")
+        faq.save()
+        self.assertEqual(faq.get_translated_question('ml'), faq.question_ml)
